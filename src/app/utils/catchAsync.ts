443c55e4cf0485c1b6117a288e0fdbba48b57ba9
Rabiul Islam <rabiul.robi2023@gmail.com>
2025-10-08 21:59:45 +0600

    catchAsync and sendResponse utils added

---
+++ src/app/utils/catchAsync.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, RequestHandler, Response } from "express";
+
+const catchAsync = (fn: RequestHandler) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await fn(req, res, next);
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+export default catchAsync;
+
+//Alternative
+
+// const catchAsync = (fn: RequestHandler) => {
+//   return (req: Request, res: Response, next: NextFunction) => {
+//     Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+//   };
+// };
+// export default catchAsync;
